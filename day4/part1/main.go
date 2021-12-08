ab4d1c7e0e464c5e4e395f708e6f6aed81acce8d
tanryberdi.syhmyradow <tanryberdi.syhmyradow@rain.us>
2021-12-08 16:11:52 +0500

    Day-4 part-1 solved

---
+++ day4/part1/main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	path := "input2.txt"
+	firstLine, matrix := readFromFile(path)
+
+	fmt.Println(walkMatrix(firstLine, matrix))
+}
+
+func walkMatrix(numbers []int, matrix map[int][][]int) int {
+	walked := make(map[int][][]int)
+	for i := 0; i < len(matrix); i++ {
+		walked[i] = [][]int{
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0},
+		}
+	}
+
+	//numWins := 0
+	//boardsWin := make([]int, 0)
+
+	for i := 0; i < len(numbers); i++ {
+		for k := 0; k < len(matrix); k++ {
+			for l := 0; l < 5; l++ {
+				for m := 0; m < 5; m++ {
+					if matrix[k][l][m] == numbers[i] {
+						walked[k][l][m] = 1
+
+						// check for vertical
+						nowX := l
+						bb := true
+						for nowX+1 <= 5 {
+							bb = bb && (walked[k][nowX][m] == 1)
+							nowX++
+						}
+
+						nowX = l
+						for nowX >= 0 {
+							bb = bb && (walked[k][nowX][m] == 1)
+							nowX--
+						}
+
+						if bb {
+							//fmt.Println("Vertical came")
+							sumUnmarked := 0
+							for ii := 0; ii < 5; ii++ {
+								for jj := 0; jj < 5; jj++ {
+									if walked[k][ii][jj] == 0 {
+										sumUnmarked += matrix[k][ii][jj]
+									}
+								}
+							}
+
+							return numbers[i] * sumUnmarked
+
+						}
+
+						// check for horizontal
+						nowY := m
+						bb = true
+						for nowY+1 <= 5 {
+							bb = bb && (walked[k][l][nowY] == 1)
+							//fmt.Println(walked[k][l][nowY], " ", bb)
+							nowY++
+						}
+						//fmt.Println(bb)
+
+						nowY = m
+						for nowY >= 0 {
+							bb = bb && (walked[k][l][nowY] == 1)
+							//fmt.Println(walked[k][l][nowY], " ", bb)
+							//fmt.Println(l, " ", nowY)
+							nowY--
+						}
+						//fmt.Println(bb)
+
+						if bb {
+							//fmt.Println("Horizontal came")
+							sumUnmarked := 0
+							for ii := 0; ii < 5; ii++ {
+								for jj := 0; jj < 5; jj++ {
+									if walked[k][ii][jj] == 0 {
+										sumUnmarked += matrix[k][ii][jj]
+									}
+								}
+							}
+
+							return numbers[i] * sumUnmarked
+
+						}
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println(walked)
+	return 0
+}
+
+// Converting strings to 2D int array for each matrix
+func matrixStTo2DArrayInt(matrix []string) [][]int {
+	newMatrix := make([][]int, 0)
+
+	for _, st := range matrix {
+		numbers := make([]int, 0)
+		nums := strings.Split(st, " ")
+		for _, num := range nums {
+			numb, _ := strconv.Atoi(num)
+			numbers = append(numbers, numb)
+		}
+		newMatrix = append(newMatrix, [][]int{numbers}...)
+	}
+
+	return newMatrix
+}
+
+// Reading input file ...
+func readFromFile(path string) (numbers []int, matrix map[int][][]int) {
+	var firstLine string
+	matrix = make(map[int][][]int)
+	numOfMatrix := 0
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	line := 0
+	var matrixSt []string
+	for scanner.Scan() {
+		st := scanner.Text()
+
+		if line == 0 {
+			firstLine = strings.TrimSpace(st)
+			nums := strings.Split(firstLine, ",")
+			for _, num := range nums {
+				numb, _ := strconv.Atoi(num)
+				numbers = append(numbers, numb)
+			}
+		} else {
+			if st != "" {
+				st = strings.ReplaceAll(st, "  ", " ")
+				st = strings.TrimLeft(st, " ")
+				st = strings.TrimRight(st, " ")
+				//fmt.Println(st)
+				matrixSt = append(matrixSt, st)
+				if len(matrixSt) == 5 {
+					//fmt.Println(matrixStTo2DArrayInt(matrixSt))
+					matrix[numOfMatrix] = matrixStTo2DArrayInt(matrixSt)
+					numOfMatrix++
+					matrixSt = nil
+				}
+			}
+		}
+
+		line++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return numbers, matrix
+}
